43134ffd9ad6f22c28c0567685fd3a84629cf02a
aioa <maaltarifi97@gmail.com>
2026-09-12 14:30:15 +0300

    slugify: separator handling

--- src/slugify.js
+++ src/slugify.js
@@ -13,7 +13,7 @@ function slugify(input) {
     .normalize('NFKD')
     .replace(/[\u0300-\u036f]/g, '')
     .toLowerCase()
-    .replace(/[^a-z0-9]+/g, '-');
+    .replace(/[^a-z0-9]/g, '-');
 }
 
 module.exports = { slugify };
