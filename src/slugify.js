e5ffbe68f0e2044b0770e0b67a1f1b06c087c6c9
aetheltech-lab <aetheltech@gmail.com>
2026-09-13 15:12:29 +0300

    Update slugify.js

--- src/slugify.js
+++ src/slugify.js
@@ -13,7 +13,9 @@ function slugify(input) {
     .normalize('NFKD')
     .replace(/[\u0300-\u036f]/g, '')
     .toLowerCase()
-    .replace(/[^a-z0-9]+/g, '-').replace(/^-+|-+$/g, '');
+    .trim()
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/^-+|-+$/g, '');
 }
 
 module.exports = { slugify };
